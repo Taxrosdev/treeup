7932db39ed2737e3dfbb448a680bcd0323155046
Taxrosdev <taxrosdev@mailbox.org>
2026-07-24 12:55:43 +1200

    chore!: remove legacy Repo::downloader

--- src/repo/mod.rs
+++ src/repo/mod.rs
@@ -1,9 +1,6 @@
 use std::path::PathBuf;
 
-use crate::downloader::Downloader;
-
 pub struct Repo {
     pub objects_path: PathBuf,
     pub blobs_path: PathBuf,
-    pub downloader: Box<dyn Downloader>,
 }
