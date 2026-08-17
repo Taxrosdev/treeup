19736efa989c9cb782b4cac15254202ec2aa3488
Taxrosdev <taxrosdev@mailbox.org>
2026-08-17 13:24:39 +1200

    feat: add DownloaderExt

--- src/downloader/mod.rs
+++ src/downloader/mod.rs
@@ -2,3 +2,27 @@ mod reqwest;
 pub use reqwest::*;
 mod progress;
 pub use progress::*;
+
+use tokio_stream::StreamExt;
+use treeup_core::downloader::{DownloadError, DownloadKind, Downloader};
+
+pub trait DownloaderExt {
+    fn fetch_string(
+        &self,
+        hash: &[u8],
+        kind: DownloadKind,
+    ) -> impl Future<Output = Result<String, DownloadError>> + Send;
+}
+
+impl<D: Downloader> DownloaderExt for D {
+    async fn fetch_string(&self, hash: &[u8], kind: DownloadKind) -> Result<String, DownloadError> {
+        let mut fetch = self.fetch(hash, kind).await?;
+        let mut data = Vec::new();
+
+        while let Some(chunk) = fetch.next().await {
+            data.extend_from_slice(&chunk?);
+        }
+
+        Ok(String::from_utf8(data)?)
+    }
+}
