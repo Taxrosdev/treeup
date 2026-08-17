61c05c45d1a2e087b74af94984328ee70ab0f9ea
Taxrosdev <taxrosdev@mailbox.org>
2026-08-17 13:21:45 +1200

    fix(core)!: ObjectCAS trait requires Send + Sync

--- core/src/object_cas.rs
+++ core/src/object_cas.rs
@@ -1,6 +1,6 @@
 use std::io;
 
-pub trait ObjectCAS {
+pub trait ObjectCAS: Send + Sync {
     fn get(&self, hash: &[u8]) -> impl Future<Output = io::Result<String>> + Send;
 
     fn exists(&self, hash: &[u8]) -> impl Future<Output = io::Result<bool>> + Send;
